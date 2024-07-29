c42eadf61a852ff6b86282c6e4dcf0631d2c512d
Mustafa S <mustafasahin@std.iyte.edu.tr>
2024-07-29 15:52:56 +0300

    Sing-In page - Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 // import Todos from './components/todos'
-import SignUp from './components/todos/Sign-Up/SignUp';
+// import SignUp from './components/todos/Sign-Up/SignUp';
+import SignIn from './components/todos/Sign-In/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <SignIn />
     </div>
   );
 }
